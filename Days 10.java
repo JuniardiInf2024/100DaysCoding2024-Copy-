2a08396b30de00670e9287824f08140662693712
JuniardiInf2024 <juniardisalulemo@gmail.com>
2024-09-11 13:23:39 +0800

    Create Days 10.java
    Menampilkan Type Data: Byte

---
+++ Days 10.java
@@ -0,0 +1,16 @@
+package Days10;
+
+public class Days10 {
+    public static void main(String[] args) {
+        
+        byte minimum = -128;
+        byte maksimum = 127;
+
+        System.out.println("Nilai minimum dari tipe data byte: " + minimum);
+        System.out.println("Nilai maksimum dari tipe data byte: " + maksimum);
+        
+        byte umur = 25;
+        
+        System.out.println("Umur: " + umur);
+    }
+}
