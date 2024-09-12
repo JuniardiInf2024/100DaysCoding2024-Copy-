aca850ddaa60afd452c7298e97d7aa7886abc235
JuniardiInf2024 <juniardisalulemo@gmail.com>
2024-09-12 08:31:07 +0800

    Create Days 11.java
    Menampilkan Type Data Short

---
+++ Days 11.java
@@ -0,0 +1,17 @@
+package days11;
+
+public class days11 {
+    
+    public static void main(String[] args) {
+        
+        short minimum = -32768;
+        short maksimum = 32767;
+
+        System.out.println("Nilai minimum dari tipe data short: " + minimum);
+        System.out.println("Nilai maksimum dari tipe data short: " + maksimum);
+        
+        short tahun = 2024;
+        
+        System.out.println("tahun: " + tahun);
+    }
+}
